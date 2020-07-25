6eb983c0ff3b379aeb66311a62fd381f22e83be6
Jakub Magáč <jakub.magac@student.tuke.sk>
2020-07-25 20:53:11 +0200

    Updates

---
+++ precache-manifest.8d8b69446413e88eb3576d9b18bc152e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b9959043ad30c87c8b79e4777f3a7a",
+    "url": "/robo-frineds-clone/index.html"
+  },
+  {
+    "revision": "2b71f605dfc6ba4f8fa3",
+    "url": "/robo-frineds-clone/static/css/main.e53200a2.chunk.css"
+  },
+  {
+    "revision": "5baa0661484ed340d3ac",
+    "url": "/robo-frineds-clone/static/js/2.6be0f9b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-frineds-clone/static/js/2.6be0f9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b71f605dfc6ba4f8fa3",
+    "url": "/robo-frineds-clone/static/js/main.a7b8661d.chunk.js"
+  },
+  {
+    "revision": "e96da8bf58b728e62ece",
+    "url": "/robo-frineds-clone/static/js/runtime-main.ab148051.js"
+  },
+  {
+    "revision": "93d7d8fe3bc59d63141bd6908415f232",
+    "url": "/robo-frineds-clone/static/media/background.93d7d8fe.jpg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/robo-frineds-clone/static/media/get_schwifty.4b4420cf.ttf"
+  }
+]);
